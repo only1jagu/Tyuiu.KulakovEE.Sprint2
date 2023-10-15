51713e4a7c6a381a432386034c7f14f20dbff901
Егор Кулаков <egorkakylakov2000@mail.ru>
2023-10-15 20:31:47 +0500

    Task2

---
+++ Tyuiu.KulakovEE.Sprint2.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KulakovEE.Sprint2.Task2.V27.Lib;
+
+namespace Tyuiu.KulakovEE.Sprint2.Task2.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 4, y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
